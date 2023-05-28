30c273d3e5396ba40b39d22bc4f50b60210ce0d8
Sandra Fernandz <sandraenmunich@gmail.com>
2023-05-28 08:24:32 +0200

    added routing

--- app.js
+++ app.js
@@ -8,11 +8,19 @@ console.log(infoCursos);
 //Routing
 
 app.get('/', (req, res) => {
-  res.send('Mi primer servidor de 💻 Cursos 💻');
+  res.send('Mi primer servidor de 💻 Cursos 💻 con Express');
+});
+
+app.get('/api/cursos', (req, res) => {
+  res.send(infoCursos);
+});
+
+app.get('/api/cursos/programacion', (req, res) => {
+  res.send(infoCursos.programacion);
 });
 
 const PUERTO = process.env.PORT || 3000;
 
 app.listen(PUERTO, () => {
-  console.log(`El servidor esta escuchando en el puerto ${PUERTO}`);
+  console.log(`El servidor esta escuchando en el puerto ${PUERTO}...`);
 });
