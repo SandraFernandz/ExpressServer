8a671fba2c0fb0bb092e678c1e328b07c1c6c563
Sandra Fernandz <sandraenmunich@gmail.com>
2023-05-28 07:43:14 +0200

    created basic serve and data base

---
+++ cursos.js
@@ -0,0 +1,48 @@
+let infoCursos = {
+  programacion: [
+    {
+      id: 1,
+      titulo: 'Aprende Python',
+      lenguaje: 'python',
+      vistas: '15000',
+      nivel: 'basico',
+    },
+    {
+      id: 2,
+      titulo: 'Aprende Javascript',
+      lenguaje: 'javascript',
+      vistas: '12900',
+      nivel: 'intermedio',
+    },
+    {
+      id: 3,
+      titulo: 'Aprende C++',
+      lenguaje: 'c',
+      vistas: '9800',
+      nivel: 'basico',
+    },
+  ],
+  matematicas: [
+    {
+      id: 1,
+      titulo: 'Aprende Calculo',
+      lenguaje: 'calculo',
+      vistas: '11768',
+      nivel: 'basico',
+    },
+    {
+      id: 2,
+      titulo: 'Aprende Algebra',
+      lenguaje: 'algebra',
+      vistas: '13090',
+      nivel: 'intermedio',
+    },
+    {
+      id: 3,
+      titulo: 'Aprende Aritmetica',
+      lenguaje: 'aritmetica',
+      vistas: '97886',
+      nivel: 'basico',
+    },
+  ],
+};
